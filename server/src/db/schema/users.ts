59043b416270da9efeebea84e04f2492eb79536e
mikhaelmounay <mikhaelmounay@gmail.com>
2024-11-05 13:00:30 +0200

    server: added User schema + split npm dev, db:generate, and db:migrate scripts

--- server/src/db/schema/users.ts
+++ server/src/db/schema/users.ts
@@ -1,7 +1,19 @@
-import { integer, pgTable, timestamp } from "drizzle-orm/pg-core";
+import { char, pgEnum, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
 
+// Define the role enum
+export const roleEnum = pgEnum("role", ["admin", "instructor", "student"]);
+
+// Define the users table schema
 export const usersTable = pgTable("users", {
-    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    id: serial().primaryKey(),
+    aucId: text().unique().notNull(),
+    name: text().notNull(),
+    email: text().unique().notNull(),
+    hashedPassword: char({length: 64}).notNull(),
+    role: roleEnum().notNull(),
+    department: text(),
+    classStanding: text(),
+    major: text(),
     updatedAt: timestamp(),
     createdAt: timestamp().defaultNow().notNull(),
     deletedAt: timestamp(),
