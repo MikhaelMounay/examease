1c4196a9ea31bf660cbda7f6a5d1ec80e278860c
omarramy22 <omarramy@aucegypt.edu>
2024-11-19 21:45:23 +0200

    lolen

--- web-client/src/pages/CourseInfoView.tsx
+++ web-client/src/pages/CourseInfoView.tsx
@@ -43,7 +43,6 @@ const CourseInfoPage: React.FC = () => {
       <button className="action-button1" onClick={()=> navigate('/create-course')}>
                     Edit Course Properties
                 </button>
-      
     </div>
   );
 
@@ -102,20 +101,6 @@ const CourseInfoPage: React.FC = () => {
     if (!courseInfo) return <p>No course data available.</p>;
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const renderCourseInfo = () => {
-    if (userData?.role === 'INSTRUCTOR' && course) {
-      return (
-        <div className="course-info">
-          <h3>Course Details</h3>
-          <p><strong>Course Title:</strong> {course.title}</p>
-          <p><strong>Enrollment Code:</strong> {course.enrollmentKey}</p>
-          <p><strong>Number of Students:</strong> {course.numStudents}</p>
-        </div>
-      );
-    }
-    return null;
-  };
-
     return (
         <div>
             <div className="course-info-container">
