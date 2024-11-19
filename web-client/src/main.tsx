9c6110f7d48ca7bbf3f5541c921dce15ef36b446
mikhaelmounay <mikhaelmounay@gmail.com>
2024-11-19 17:05:49 +0200

    (fix) web-client: added missing import in src/main.tsx

--- web-client/src/main.tsx
+++ web-client/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PrimeReactProvider } from "primereact/api";
+import {CourseProvider} from "./contexts/CourseContext.tsx";
 
 import router from "./router";
 
