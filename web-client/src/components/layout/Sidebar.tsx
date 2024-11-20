9928ee59b4fdc6cc2c7704fa49af2b7439de6649
mikhaelmounay <mikhaelmounay@gmail.com>
2024-11-20 19:57:13 +0200

    (fix) web-client: Conditionally-rendered nav links in Sidebar for different User Roles

--- web-client/src/components/layout/Sidebar.tsx
+++ web-client/src/components/layout/Sidebar.tsx
@@ -15,13 +15,14 @@ import { useAuth } from "../../contexts/AuthWrapper.tsx";
 import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
 import SidebarItem from "./SidebarItem.tsx";
 import { createPortal } from "react-dom";
+import { Role } from "../../types/User.ts";
 
 type SidebarProps = {
     isSidebarOpen: boolean;
     setIsSidebarOpen: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-const Sidebar: React.FC<SidebarProps> = function (props) {
+const Sidebar: React.FC<SidebarProps> = function(props) {
     // Constants
     const sidebarWidthConstraints = {
         minWidth: 192, // 12rem
@@ -44,26 +45,35 @@ const Sidebar: React.FC<SidebarProps> = function (props) {
     const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
     const [sidebarWidth, setSidebarWidth] = useState(sidebarWidthConstraints.defaultWidth);
     const [isResizing, setIsResizing] = useState(false);
-    const [sidebarItems, _setSidebarItems] = useState<{ linkTo: string; icon: IconDefinition; label: string }[]>([
+    const [sidebarItems, _setSidebarItems] = useState<{
+        linkTo: string;
+        icon: IconDefinition;
+        label: string;
+        rolesEnabled: Role[]
+    }[]>([
         {
             linkTo: "/",
             icon: faHome,
             label: "Home",
+            rolesEnabled: ["STUDENT", "ADMIN", "INSTRUCTOR"],
         },
         {
             linkTo: "courses",
             icon: faBook,
             label: "Courses",
+            rolesEnabled: ["STUDENT", "ADMIN", "INSTRUCTOR"],
         },
         {
             linkTo: "/create-course",
             icon: faFolderPlus,
             label: "Create Course",
+            rolesEnabled: ["ADMIN", "INSTRUCTOR"],
         },
         {
             linkTo: " ",
             icon: faBullhorn,
             label: "Announcments",
+            rolesEnabled: ["STUDENT", "ADMIN", "INSTRUCTOR"],
         },
     ]);
 
@@ -105,8 +115,10 @@ const Sidebar: React.FC<SidebarProps> = function (props) {
         window.addEventListener("resize", handleResize);
 
         return () => {
-            window.removeEventListener("mousemove", (_e) => {});
-            window.removeEventListener("mouseup", () => {});
+            window.removeEventListener("mousemove", (_e) => {
+            });
+            window.removeEventListener("mouseup", () => {
+            });
             window.removeEventListener("resize", handleResize);
         };
     }, []);
@@ -122,14 +134,15 @@ const Sidebar: React.FC<SidebarProps> = function (props) {
     return (
         <>
             {props.isSidebarOpen && (
-                <div className="text-textColorLight absolute left-0 top-0 z-30 bg-black shadow-lg shadow-gray-600 lg:relative lg:flex lg:bg-opacity-85">
+                <div
+                    className="text-textColorLight absolute left-0 top-0 z-30 bg-black shadow-lg shadow-gray-600 lg:relative lg:flex lg:bg-opacity-85">
                     {props.isSidebarOpen &&
                         createPortal(
                             <div
                                 className="absolute left-0 top-0 z-20 h-full w-full bg-gray-900 bg-opacity-50 lg:hidden"
                                 onClick={() => props.setIsSidebarOpen(false)}
                             ></div>,
-                            document.getElementById("backdrop")!
+                            document.getElementById("backdrop")!,
                         )}
 
                     <aside
@@ -139,8 +152,8 @@ const Sidebar: React.FC<SidebarProps> = function (props) {
                                 window.innerWidth < breakpointsTailwind.lg
                                     ? "fit-content"
                                     : isSidebarCollapsed
-                                      ? "5rem"
-                                      : `${sidebarWidth / 16}rem`,
+                                        ? "5rem"
+                                        : `${sidebarWidth / 16}rem`,
                         }}
                     >
                         <div className="flex h-full flex-col justify-between">
@@ -159,14 +172,15 @@ const Sidebar: React.FC<SidebarProps> = function (props) {
                                         {isSidebarCollapsed && <FontAwesomeIcon icon={faAngleRight} />}
                                         {!isSidebarCollapsed && <FontAwesomeIcon icon={faAngleLeft} />}
                                     </button>
-                                    <button className="ms-2 p-2 lg:hidden" onClick={() => props.setIsSidebarOpen(false)}>
+                                    <button className="ms-2 p-2 lg:hidden"
+                                            onClick={() => props.setIsSidebarOpen(false)}>
                                         <FontAwesomeIcon icon={faXmark} />
                                     </button>
                                 </div>
 
                                 <nav className="max-h-[calc(100%-120px)] overflow-y-auto overflow-x-hidden">
                                     <ul>
-                                        {sidebarItems.map((item, i) => (
+                                        {sidebarItems.filter(item => item.rolesEnabled.includes(userData?.role || "STUDENT")).map((item, i) => (
                                             <SidebarItem
                                                 key={i}
                                                 linkTo={item.linkTo}
@@ -218,7 +232,8 @@ const Sidebar: React.FC<SidebarProps> = function (props) {
                                             </div>
                                         </button>
                                         {isSidebarCollapsed && (
-                                            <div className="bg-primary text-textColorLight invisible absolute left-full top-2 ml-8 -translate-x-3 truncate rounded-md px-2 py-1 text-sm opacity-20 transition-all group-hover:visible group-hover:translate-x-0 group-hover:opacity-100">
+                                            <div
+                                                className="bg-primary text-textColorLight invisible absolute left-full top-2 ml-8 -translate-x-3 truncate rounded-md px-2 py-1 text-sm opacity-20 transition-all group-hover:visible group-hover:translate-x-0 group-hover:opacity-100">
                                                 Logout
                                             </div>
                                         )}
