61125fb416f751c78b94ae244aa3ee6db1cb0dba
mikhaelmounay <mikhaelmounay@gmail.com>
2024-12-01 10:17:21 +0200

    (feat) desktop-client: disabled running on VMs, disabled using clipboard while the app is open, running app in Kiosk mode by default w/ fullscreen & alwaysOnTop enabled, enabled electron contentProtection

--- desktop-client/src/preload/index.ts
+++ desktop-client/src/preload/index.ts
@@ -1,24 +1,27 @@
 import { contextBridge, ipcRenderer } from "electron";
-import { electronAPI } from "@electron-toolkit/preload";
+
+// electronAPI
+const electronAPI = {
+  send: (channel: string, ...args: any[]) => ipcRenderer.invoke(channel, ...args),
+  on: (channel: string, listener: (...args: any[]) => void) =>
+    ipcRenderer.on(channel, (_event, ...args) => listener(...args)),
+};
 
 // Custom APIs for renderer
 const api = {
-    getConfirmationFromDesktopApp: () => ipcRenderer.invoke("getConfirmationFromDesktopApp"),
+  getConfirmationFromDesktopApp: () => ipcRenderer.invoke("getConfirmationFromDesktopApp"),
 };
 
 // Use `contextBridge` APIs to expose Electron APIs to
 // renderer only if context isolation is enabled, otherwise
 // just add to the DOM global.
-if (process.contextIsolated) {
-    try {
-        contextBridge.exposeInMainWorld("electron", electronAPI);
-        contextBridge.exposeInMainWorld("api", api);
-    } catch (error) {
-        console.error(error);
-    }
-} else {
-    // @ts-ignore (define in dts)
-    window.electron = electronAPI;
-    // @ts-ignore (define in dts)
-    window.api = api;
+if (!process.contextIsolated) {
+  throw new Error("contextIsolation is disabled");
+}
+
+try {
+  contextBridge.exposeInMainWorld("electronAPI", electronAPI);
+  contextBridge.exposeInMainWorld("api", api);
+} catch (error) {
+  console.error(error);
 }
